d41cc19a1fc97d38ec229220ebf8b72d26feff8e
dagbru <dag.sorensen@gmail.com>
2024-12-03 13:17:16 +0100

    added url downloader

---
+++ ExceptionHandling/WebsiteDownloader.cs
@@ -0,0 +1,14 @@
+﻿namespace ExceptionHandling;
+
+public class WebsiteDownloader
+{
+    public string Download(string url)
+    {
+        using var client = new HttpClient();
+        var response = client.GetAsync(url).Result;
+        response.EnsureSuccessStatusCode();
+        var responseBody = response.Content.ReadAsStringAsync().Result;
+
+        return responseBody;
+    }
+}
