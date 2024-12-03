f43a3990a9bf41fc2af746cc181c6647ed51c82d
dagbru <dag.sorensen@gmail.com>
2024-12-03 11:50:03 +0100

    initial commit

---
+++ ExceptionHandling/FileManager.cs
@@ -0,0 +1,36 @@
+﻿namespace ExceptionHandling;
+
+public class FileManager
+{
+    public string Path { get; }
+
+    public FileManager(string path)
+    {
+        Path = path;
+    }
+    
+    public string ReadFileContent()
+    {
+        return File.ReadAllText(Path);
+    }
+
+    public void WriteFileContent(string content)
+    {
+        File.WriteAllText(Path, content);
+    }
+
+    public void CreateFile()
+    {
+        File.Create(Path).Close();
+    }
+
+    public void DeleteFile()
+    {
+        File.Delete(Path);
+    }
+
+    public void MoveFile(string newPath)
+    {
+        File.Move(Path, newPath);
+    }
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using ExceptionHandling;
+
+var fileManager = new FileManager(@"C:\Dev\test.txt");
+
+Console.WriteLine("Available commands: create, write, read, delete, move");
+while (true)
+{
+    Console.Write("File command: ");
+    var command = Console.ReadLine();
+    switch (command)
+    {
+        case "read":
+            Console.WriteLine(fileManager.ReadFileContent());
+            break;
+        case "create":
+            fileManager.CreateFile();
+            Console.WriteLine($"File {fileManager.Path} created");
+            break;
+        case "write":
+            Console.Write("Content: ");
+            var content = Console.ReadLine();
+            fileManager.WriteFileContent(content);
+            break;
+        case "delete":
+            fileManager.DeleteFile();
+            break;
+        case "move":
+            Console.Write("New path: ");
+            var newPath = Console.ReadLine();
+            fileManager.MoveFile(newPath);
+            break;
+        case "exit":
+            Environment.Exit(0);
+            break;
+    }
+}
+
+
+
+
